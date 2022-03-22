9a7d7c333461b182fc7078c9238aeae01ccfe58f
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-03-22 18:14:23 +0530

    seo push

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "./Landing.css";
 import mes from "./../../assets/mesLogo.png";
+import { Helmet } from 'react-helmet';
 import Timeline from "../../components/Timeline/Timeline";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Landing = () => {
     return (
         <div className="page-wrapper">
+            <Helmet>
+                <title>MES 2022 | E-Cell MIT</title>
+                <meta name='description' content='Manipal Entrepreneurship Summit 2022 organised by E-Cell MIT' />
+                <meta name='theme-color' content='#EFEFEF' />
+                <meta
+                  name='keywords'
+                  content='mes, entrepreneurship, summit, ecell, manipal institute of technology, startup, pitch tank, shark tank'
+                />
+                <meta name='url' content='http://mes.ecellmit.com/' />
+                <meta name='coverage' content='Worldwide' />
+                <meta name='target' content='all' />
+                <meta name='HandheldFriendly' content='True' />
+                <link rel="canonical" href="http://mes.ecellmit.com/" />
+                {/* OG meta tags */}
+                <meta property="og:type" content="webpage" />
+                <meta
+                  property="og:title"
+                  content="MES 2022 | E-Cell MIT"
+                />
+                <meta
+                  property="og:description"
+                  content="Manipal Entrepreneurship Summit 2022 organised by E-Cell MIT"
+                />
+                <meta property="og:image" content='' />
+                <meta property="og:url" content='http://mes.ecellmit.com/' />
+                <meta
+                  property="og:site_name"
+                  content="MES 2022 | E-Cell MIT"
+                />
+                <meta
+                  name='twitter:title'
+                  content='Manipal Entrepreneurship Summit 2022 organised by E-Cell MIT'
+                />
+                <meta
+                  name="twitter:description"
+                  content="Manipal Entrepreneurship Summit 2022 organised by E-Cell MIT"
+                />
+            </Helmet>
             <div className="gradient-wrapper">
             <div className="content-wrapper">
                 <Navbar />
